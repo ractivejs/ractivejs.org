ebb75c39a16373b6944465cbea7e01e8b1f314a9
Rich Harris <richard.a.harris@gmail.com>
2014-04-03 15:08:23 -0400

    building front page and main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 require([
+	'ractive',
+	'rvc!components/helloworld',
+
 	'shared/utils/nav'
-]);
+], function ( Ractive, HelloWorld ) {
+
+	'use strict';
+
+	window.Ractive = Ractive;
+
+	new HelloWorld({ el: '[data-component="hello-world"]' });
+});
